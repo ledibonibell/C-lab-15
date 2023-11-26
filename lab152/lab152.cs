16c0e15742374046e988a9b3133f417ff7eaf694
Egor Devyatkin <inkey.cherry@gmail.com>
2023-11-26 14:45:31 +0300

    Добавьте файлы проекта.

---
+++ lab152/lab152.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+public interface IRepository
+{
+    void Save(string data);
+}
+
+public class FileRepository : IRepository
+{
+    private readonly string filename;
+
+    public FileRepository(string filename)
+    {
+        this.filename = filename;
+    }
+
+    public void Save(string data)
+    {
+        File.AppendAllText(filename, data + Environment.NewLine);
+    }
+}
+
+public class JsonRepository : IRepository
+{
+    private readonly string filename;
+
+    public JsonRepository(string filename)
+    {
+        this.filename = filename;
+    }
+
+    public void Save(string data)
+    {
+        File.AppendAllText(filename, JsonConvert.SerializeObject(data) + Environment.NewLine);
+    }
+}
+
+public class MyLogger
+{
+    private readonly IRepository[] repositories;
+
+    public MyLogger(params IRepository[] repositories)
+    {
+        this.repositories = repositories;
+    }
+
+    public void Log(string message)
+    {
+        foreach (var repository in repositories)
+        {
+            repository.Save(message);
+        }
+    }
+}
+
+class lab152
+{
+    static void Main()
+    {
+        var fileRepo = new FileRepository("lab152/log.txt");
+        var jsonRepo = new JsonRepository("lab152/log.json");
+
+        var logger = new MyLogger(fileRepo, jsonRepo);
+
+        Console.Write("Enter a message for the log:");
+        string logMessage = Console.ReadLine();
+
+        logger.Log(logMessage);
+
+        Console.WriteLine("The log is written to files");
+    }
+}
